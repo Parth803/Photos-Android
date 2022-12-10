2ee1761230e1a689bef5877c16b361b4c8867467
Parth Patel <parthp1217@gmail.com>
2022-12-10 15:49:45 -0500

    sponsored by geico

--- app/src/main/java/com/example/android10/HomeAdapter.java
+++ app/src/main/java/com/example/android10/HomeAdapter.java
@@ -1,6 +1,9 @@
 package com.example.android10;
 
 import model.Album;
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -17,46 +20,55 @@ import java.util.List;
 public class HomeAdapter extends
         RecyclerView.Adapter<HomeAdapter.ViewHolder> {
 
-    private List<Album> userAlbums;
-
-    // Pass in the contact array into the constructor
-    public HomeAdapter(List<Album> albums) {
-        userAlbums = albums;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    // Provide a direct reference to each of the views within a data item
-    // Used to cache the views within the item layout for fast access
     public class ViewHolder extends RecyclerView.ViewHolder {
-        // Your holder should contain a member variable
-        // for any view that will be set as you render a row
+
         public TextView albumNameTextView;
         public Button openAlbumButton;
 
-        // We also create a constructor that accepts the entire item row
-        // and does the view lookups to find each subview
+
         public ViewHolder(View itemView) {
-            // Stores the itemView in a public final member variable that can be used
-            // to access the context from any ViewHolder instance.
+
             super(itemView);
 
             albumNameTextView = (TextView) itemView.findViewById(R.id.albumNameTextView);
             openAlbumButton = (Button) itemView.findViewById(R.id.openAlbumButton);
         }
+    }
+
+    private List<Album> userAlbums;
+
+    public HomeAdapter(List<Album> albums) {
+        userAlbums = albums;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @Override
+    public HomeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.album_card, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(HomeAdapter.ViewHolder holder, int position) {
+        // Get the data model based on position
+        Album album = userAlbums.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = holder.albumNameTextView;
+        textView.setText(album.name);
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return userAlbums.size();
     }
 }
--- app/src/main/java/com/example/android10/MainActivity.java
+++ app/src/main/java/com/example/android10/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.android10;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+import model.Album;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Album> albums;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // get albums from storage
+        albums = new ArrayList<Album>();
+        albums.add(new Album("Gay = Happy"));
+
+        // Create adapter passing in the sample user data
+        HomeAdapter adapter = new HomeAdapter(albums);
+
+        rvContacts.setAdapter(adapter);
+
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
     }
 }
