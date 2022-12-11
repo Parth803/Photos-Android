4347b067543247a9a4d2ae5407140d4cc4e2668e
YashPatel709 <yash.p.patel@rutgers.edu>
2022-12-11 13:55:48 -0500

    ok

--- app/src/main/java/model/User.java
+++ app/src/main/java/model/User.java
@@ -195,11 +195,11 @@ public final class User implements java.io.Serializable, Comparable<User> {
      * @return photos filtered by tag
      */
     public ArrayList<Photo> getPhotosByTag(String type, String value) {
-        Predicate<Photo> containsTag = p -> photoContainsTag(p, type, value);
+        Predicate<Photo> containsTag = p -> photoContainsTagWithAutoCompletedValue(p, type, value);
         return getPhotos(containsTag);
     }
 
-    public boolean photoContainsTag(Photo p, String type, String value) {
+    public boolean photoContainsTagWithAutoCompletedValue(Photo p, String type, String value) {
         for (Tag tag : p.tags) {
             String elementValue = tag.value.toLowerCase(Locale.US);
             String thisValue = value.toLowerCase(Locale.US);
@@ -220,8 +220,8 @@ public final class User implements java.io.Serializable, Comparable<User> {
      * @return photos filtered by tags
      */
     public ArrayList<Photo> getPhotosByTags(String type1, String value1, String type2, String value2, boolean isAND) {
-        Predicate<Photo> containsTag1 = p -> photoContainsTag(p, type1, value1);
-        Predicate<Photo> containsTag2 = p -> photoContainsTag(p, type2, value2);
+        Predicate<Photo> containsTag1 = p -> photoContainsTagWithAutoCompletedValue(p, type1, value1);
+        Predicate<Photo> containsTag2 = p -> photoContainsTagWithAutoCompletedValue(p, type2, value2);
         Predicate<Photo> containsTags;
         if (isAND) {
             containsTags = containsTag1.and(containsTag2);
