9745d2bfeb4d564587ed8f8933d91a58f9e9616b
Parth Patel <parthp1217@gmail.com>
2022-12-13 07:09:10 -0500

    added slideshow movement

--- app/src/main/java/activity/DisplayActivity.java
+++ app/src/main/java/activity/DisplayActivity.java
@@ -8,28 +8,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android10.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import model.Album;
 import model.Model;
 import model.Photo;
 
 public class DisplayActivity extends AppCompatActivity {
-    public Album searchResults;
+    public Album currentAlbum;
     public Photo currentPhoto;
+    public int currentIndex;
+    public Menu optionsMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        searchResults = (Album) Model.dataTransfer.get(0);
+        currentAlbum = (Album) Model.dataTransfer.get(0);
         currentPhoto = (Photo) Model.dataTransfer.get(1);
+        currentIndex = currentAlbum.photos.indexOf(currentPhoto);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        this.setTitle((searchResults.photos.indexOf(currentPhoto) + 1)+ " of " + searchResults.photos.size());
+        getMenuInflater().inflate(R.menu.displaymenu, menu);
+        optionsMenu = menu;
+        this.setTitle((currentIndex + 1) + " of " + currentAlbum.photos.size());
+        updatePrevNext();
+        updateDisplay();
+        updateTagsList();
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -39,8 +49,36 @@ public class DisplayActivity extends AppCompatActivity {
         if (id == android.R.id.home) {
             Model.initPreviousScene();
             finish();
+        } else if (id == R.id.previous) {
+            currentIndex--;
+            currentPhoto = currentAlbum.photos.get(currentIndex);
+            updatePrevNext();
+            updateDisplay();
+            updateTagsList();
+        } else if (id == R.id.next) {
+            currentIndex++;
+            currentPhoto = currentAlbum.photos.get(currentIndex);
+            updatePrevNext();
+            updateDisplay();
+            updateTagsList();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void updatePrevNext() {
+        optionsMenu.findItem(R.id.previous).setEnabled(currentAlbum.photos.indexOf(currentPhoto) != 0);
+        optionsMenu.findItem(R.id.previous).getIcon().setAlpha((currentAlbum.photos.indexOf(currentPhoto) != 0) ? 255 : 130);
+        optionsMenu.findItem(R.id.next).setEnabled(currentAlbum.photos.indexOf(currentPhoto) != (currentAlbum.photos.size() - 1));
+        optionsMenu.findItem(R.id.next).getIcon().setAlpha((currentAlbum.photos.indexOf(currentPhoto) != (currentAlbum.photos.size() - 1)) ? 255 : 130);
+    }
+
+    public void updateDisplay() {
+        this.setTitle((currentIndex + 1) + " of " + currentAlbum.photos.size());
+//        if (!currentPhoto.path.isEmpty()) displayImage.setImage(new Image("file:" + currentPhoto.path));
+    }
+
+    public void updateTagsList() {
+//        this.tagsList.setItems(FXCollections.observableList(currentPhoto.tags.stream().map(t -> t.type+"="+t.value).collect(Collectors.toList())));
     }
 }
 
